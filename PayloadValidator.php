08de262f3c598bb39ebe6b727a5a7ef20102b30b
Timothée Barray <tim@amicalement-web.net>
2016-07-28 11:26:45 +0200

    :new: Add possibility to validate json payload through jsonSchema
    JsonSchema should be provided in route like
    ```
    hotel_register:
        path: /hotels
        defaults:
            _controller: ui.controller.hotel_controller:postHotelAction,
            _jsonSchema: { request: jsonSchema/hotel_register.json }
        methods: [POST]
    ```

---
+++ PayloadValidator.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Rezzza\SymfonyRestApiJson;
+
+use JsonSchema\Validator;
+use JsonSchema\RefResolver;
+
+class PayloadValidator
+{
+    private $delegateValidator;
+
+    private $refResolver;
+
+    public function __construct(Validator $delegateValidator, RefResolver $refResolver)
+    {
+        $this->delegateValidator = $delegateValidator;
+        $this->refResolver = $refResolver;
+    }
+
+    public function validate($payload, $jsonSchemaFilename)
+    {
+        if (false === file_exists($jsonSchemaFilename)) {
+            throw new \UnexpectedValueException(sprintf('Cannot validate payload through "%s" json schema. File does not exist.', $jsonSchemaFilename));
+        }
+
+        $this->delegateValidator->check(
+            $payload,
+            $this->refResolver->resolve('file://' . realpath($jsonSchemaFilename))
+        );
+
+        if (!$this->delegateValidator->isValid()) {
+            throw InvalidPayload::withErrors($payload, $jsonSchemaFilename, $this->delegateValidator->getErrors());
+        }
+    }
+}
